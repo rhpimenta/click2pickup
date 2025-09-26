28cada17ded707a99e4b2c1c064f83647018fa4c
rhpimenta <contato@ricardopimenta.com.br>
2025-09-26 15:19:26 -0300

    Delete click2pickup directory

--- click2pickup/assets/js/admin.js
+++
@@ -1,8 +0,0 @@
-/**
- * Scripts administrativos do Click2Pickup
- */
-
-jQuery(document).ready(function($) {
-    // Placeholder para futuros scripts
-    console.log('Click2Pickup Admin loaded');
-});
--- click2pickup/assets/js/public.js
+++
@@ -1,8 +0,0 @@
-/**
- * Click2Pickup - Public JavaScript
- */
-
-jQuery(document).ready(function($) {
-    // Scripts públicos serão adicionados conforme necessário
-    console.log('Click2Pickup Public JS Loaded');
-});
